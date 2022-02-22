6bd07f351aafa155e020c67dea97120f58f9beed
Hladikes <adi.hladik@gmail.com>
2022-02-22 23:24:25 +0100

    🛠 Ugly redirect hack removed

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,5 @@
 import App from './App.svelte'
 
-if (!window.location.protocol.includes('https') && !window.location.host.includes(':')) {
-  window.location.replace(`https:${document.URL.substring(5)}`)
-}
-
 const app = new App({
   target: document.getElementById('app'),
 })
