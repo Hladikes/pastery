abca79faae492f4562eac1cfefb7101b49ac7592
Adam Hladik <Adam.Hladik@ibm.com>
2022-12-22 11:43:01 +0100

    🎨 placeholder color changed

--- src/App.svelte
+++ src/App.svelte
@@ -89,7 +89,7 @@
       bind:value={searchKeywords}
       type="text" 
       placeholder="Search"
-      class="bg-secondary/10 text-white border-2 border-transparent md:text-3xl text-2xl font-medium placeholder-gray-300  md:p-5 p-4 w-full  rounded-xl transition-colors focus:outline-none focus:bg-accent/20 focus:border-accent/40">
+      class="bg-secondary/10 text-white border-2 border-transparent md:text-3xl text-2xl font-medium placeholder-gray-300  md:p-5 p-4 w-full  rounded-xl transition-colors focus:outline-none focus:placeholder-white focus:bg-accent/20 focus:border-accent/40">
   </div>
 
   <div class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-7 gap-4 w-full p-4">
