76514c43f7b95c196b418b3c3fe1d0ae4c7b99ae
Adam Hladik <adi.hladik@gmail.com>
2023-04-17 17:59:35 +0200

    👋 imageCache removed

--- src/components/paste/Card.svelte
+++ src/components/paste/Card.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
   import { copy } from '@/plugins/clipboard'
-  import { getCachedImage } from '@/plugins/imageCache'
   import Popup from '@/plugins/popup/Popup.svelte'
   import { createToast } from '@/plugins/toast/Toast.svelte'
   import type { Link } from '@/store/links'
   import { isSelectionModeOn, toggleSelection } from '@/store/selection'
-  import { onMount } from 'svelte'
   import { scale } from 'svelte/transition'
   import Actions from './Actions.svelte'
   import KeywordsEditor from './KeywordsEditor.svelte'
@@ -14,27 +12,10 @@
 
   export let link: Link = null
 
-  let imageContainer: HTMLElement = null
   let isPreviewVisible: boolean = false
   let isEditorVisible: boolean = false
   let isImageValid: boolean = true
   let isImageLoaded: boolean = false
-
-  onMount(() => {
-    const cachedImage = getCachedImage(
-      link.url,
-      link.id,
-      'h-full w-full object-cover select-none',
-      () => {
-        isImageLoaded = true
-      },
-      () => {
-        isImageValid = false
-      },
-    )
-    
-    imageContainer?.appendChild(cachedImage)
-  })
 
   // Friendly reminder: same function is located in Actions.svelte, but
   // I found it worthless to make it reusable. Ctrl+C, Ctrl+V ftw!
@@ -61,10 +42,12 @@
   class="group bg-primary-dark shadow-lg rounded-lg h-48 relative overflow-hidden cursor-pointer focus:outline-none focus:ring-8 focus:ring-accent focus:shadow-accent focus:shadow-2xl focus:-translate-y-3 transition-transform">
   
   {#if isImageValid}
-    <div 
-      bind:this={imageContainer}
-      class="{isImageLoaded ? 'opacity-1' : 'opacity-0'} inset-0 absolute transition-opacity">
-    </div>
+    <img 
+      on:load={() => { isImageLoaded = true }} 
+      on:error={() => { isImageValid = false }}
+      class="{isImageLoaded ? 'opacity-1' : 'opacity-0'} absolute inset-0 transition-opacity h-full w-full object-cover select-none"
+      src={link.url} 
+      alt={link.keywords}>
   {:else}
     <p class="m-4 text-red-500 text-xl font-medium">Could not load the image :(</p>
   {/if}
